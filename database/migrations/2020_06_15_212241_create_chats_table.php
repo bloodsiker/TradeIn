cecb8d37670a107a8b95af0d8301fdbedf33a813
Dmitro Ovsiichuk <d.ovsiichuk@digitalscreens.com.ua>
2020-06-28 18:47:09 +0300

    fix migrate

--- database/migrations/2020_06_15_212241_create_chats_table.php
+++ database/migrations/2020_06_15_212241_create_chats_table.php
@@ -13,32 +13,32 @@ class CreateChatsTable extends Migration
      */
     public function up()
     {
-//        Schema::create('chats', function (Blueprint $table) {
-//            $table->id();
-//            $table->tinyInteger('type_chat');
-//            $table->string('name')->nullable();
-//            $table->string('uniq_id', 100);
-//        });
-//
-//        Schema::create('chat_user', function (Blueprint $table) {
-//            $table->id();
-//            $table->bigInteger('chat_id')->unsigned();
-//            $table->bigInteger('user_id')->unsigned();
-//
-//            $table->foreign('chat_id')->references('id')->on('chats')->onDelete('cascade');
-//            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-//        });
-//
-//        Schema::create('chat_messages', function (Blueprint $table) {
-//            $table->id();
-//            $table->bigInteger('chat_id')->unsigned();
-//            $table->bigInteger('user_id')->unsigned();
-//            $table->text('message');
-//            $table->timestamps();
-//
-//            $table->foreign('chat_id')->references('id')->on('chats')->onDelete('cascade');
-//            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-//        });
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('type_chat');
+            $table->string('name')->nullable();
+            $table->string('uniq_id', 100);
+        });
+
+        Schema::create('chat_user', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('chat_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+
+            $table->foreign('chat_id')->references('id')->on('chats')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+
+        Schema::create('chat_messages', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('chat_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+            $table->text('message');
+            $table->timestamps();
+
+            $table->foreign('chat_id')->references('id')->on('chats')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
 
         Schema::create('chat_message_user', function (Blueprint $table) {
             $table->id();
